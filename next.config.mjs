0de2d656bb2153d93543120c163292aa64ef00ba
Tsang <366209274@qq.com>
2024-11-20 17:12:47 +0800

    chore: 1. update nextConfig to allow file domain; 2. update favicon and meta title and description

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,16 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // development
       {
         protocol: 'https',
         hostname: 'next-horse-726.convex.cloud',
+        pathname: '/api/storage/**',
+      },
+      // production
+      {
+        protocol: 'https',
+        hostname: 'disciplined-iguana-528.convex.cloud',
         pathname: '/api/storage/**',
       },
     ],
